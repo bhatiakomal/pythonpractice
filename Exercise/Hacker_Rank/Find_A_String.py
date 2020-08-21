02c5e9391c3b7d406fe22a9a2f8a376c206059f8
komal <komalbhatia442@gmail.com>
2020-08-21 18:15:26 +0530

    Problem based on String

---
+++ Exercise/Hacker_Rank/Find_A_String.py
@@ -0,0 +1,12 @@
+def count_substring(string, sub_string):
+    counter =0
+    for i, letters in enumerate(string):
+        if sub_string in string[i:i + len(sub_string) ]:
+            counter+=1
+    return counter
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
